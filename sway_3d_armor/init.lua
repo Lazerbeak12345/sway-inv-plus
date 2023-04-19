5d0663130e83ed162c4b1bb9de948db768f39356
Nathan Fritzler <nfblaster@live.com>
2023-04-18 21:53:34 -0600

    feat!: Make use of widgets api
    BREAKING CHANGE: Many old API calls don't work anymore.

--- sway_3d_armor/init.lua
+++ sway_3d_armor/init.lua
@@ -1,46 +1,69 @@
 local has_technic = minetest.get_modpath("technic") ~= nil
-local gui = flow.widgets
+local armor = armor
+local formspec_ast = formspec_ast
+local gui = sway.widgets
 local gui_nil = gui.Spacer{expand=false}
+local widgets = {}
+sway.mods.sway_3d_armor = { widgets = widgets }
+function widgets.List(fields)
+	local name = fields.player_name
+	local w = fields.w
+	local h = fields.h
+	local location = "detached:" .. name .. "_armor"
+	return gui.VBox{
+		gui.List{
+			inventory_location = location,
+			list_name = "armor",
+			w = w, h = h,
+		},
+		gui.Listring{
+			inventory_location = location,
+			list_name = "armor"
+		},
+		gui.Listring{
+			inventory_location = "current_player",
+			list_name = "main"
+		}
+	}
+end
+function widgets.Preview(fields)
+	local name = fields.player_name
+	return gui.Image{
+		w = 2, h = 4,
+		texture_name = armor.textures[name].preview
+	}
+end
+function widgets.Stats(fields)
+	local name = fields.player_name
+	return gui.VBox{
+		name = "sway_3d_armor_stats_box",
+		gui.Label{ label = "Stats" },
+		gui.Label{ label = "Level: " .. armor.def[name].level },
+		gui.Label{ label = "Heal: " .. armor.def[name].heal },
+		armor.config.fire_protect and gui.Label{
+			label = "Fire: " .. armor.def[name].fire
+		} or gui_nil,
+		has_technic and gui.Label{
+			label = "Radiation: " .. armor.def[name].groups["radiation"]
+		} or gui_nil
+	}
+end
 sway.register_page("sway_3d_armor:3d_armor", {
 	title = "Armor",
-	-- SUPER easy way to add new labels or stats
-	stats_box_filter = function (self, stats_box)
-		return stats_box
-	end,
-	slot_w = 2,
-	slot_h = 3,
+	slot_w = 2, slot_h = 3,
 	get = function(self, player, context)
 		local name = player:get_player_name()
-		local location = "detached:" .. name .. "_armor"
-		return sway.widgets.form{
+		return gui.sway.Form{
 			player = player,
 			context = context,
 			show_inv = true,
 			gui.HBox{
-				gui.VBox{
-					gui.List{
-						inventory_location = location,
-						list_name = "armor",
-						w = self.slot_w, h = self.slot_h,
-					},
-					gui.Listring{
-						inventory_location = location,
-						list_name = "armor"
-					},
-					gui.Listring{
-						inventory_location = "current_player",
-						list_name = "main"
-					},
+				gui.sway_3d_armor.List{
+					player_name = name,
+					w = self.slot_w, h = self.slot_h
 				},
-				gui.Image{ w = 2, h = 4, texture_name = armor.textures[name].preview },
-				self:stats_box_filter(gui.VBox{
-					name = "sway_3d_armor_stats_box",
-					gui.Label{ label = "Stats" },
-					gui.Label{ label = "Level: " .. armor.def[name].level },
-					gui.Label{ label = "Heal: " .. armor.def[name].heal },
-					armor.config.fire_protect and gui.Label{ label = "Fire: " .. armor.def[name].fire } or gui_nil,
-					has_technic and gui.Label{ label = "Radiation: " .. armor.def[name].groups["radiation"] } or gui_nil,
-				})
+				gui.sway_3d_armor.Preview{ player_name = name },
+				gui.sway_3d_armor.Stats{ player_name = name }
 			}
 		}
 	end
--- sway_awards/init.lua
+++ sway_awards/init.lua
@@ -1,7 +1,22 @@
-local gui = flow.widgets
+local sway = sway
+local awards = awards
+local gui = sway.widgets
 local gui_nil = gui.Spacer{expand=false}
-local function generate_award(self, title, description, unlocked, icon, progress)
+sway.mods.sway_awards = { widgets = {} }
+local widgets = sway.mods.sway_awards.widgets
+function widgets.Award(fields)
+	local bar_width = fields.bar_width
+	local award = fields.award
+
+	local def = award.def
+	local title = def.title
+	local description = def.description
+	local icon = def.icon
+	local unlocked = award.unlocked
+
+	local progress = award.progress
 	local current, target
+	local started = award.started
 	if progress then
 		current, target = progress.current, progress.target
 	end
@@ -14,19 +29,19 @@ local function generate_award(self, title, description, unlocked, icon, progress
 			gui.Label { label = description },
 			progress and gui.Stack {
 				gui.Box{
-					w = self.bar_width,
+					w = bar_width,
 					h = 1,
 					color = "#333333",
 					align_h = "left",
 				},
 				current > 0 and gui.Box{
-					w = (current * self.bar_width) / target,
+					w = (current * bar_width) / target,
 					h = 1,
 					color = "#008800",
 					align_h = "left",
 				} or gui_nil,
 				gui.HBox{
-					w = self.bar_width,
+					w = bar_width,
 					align_h = "left",
 					padding = .2,
 					gui.Label{
@@ -39,27 +54,43 @@ local function generate_award(self, title, description, unlocked, icon, progress
 						align_h = "right",
 					}
 				}
-			} or gui.Label{ label = "Complete!" }
-		},
+			} or unlocked and gui.Label{ label = "Complete!" }
+			or (not started) and gui.Label{ label = "Not started!" }
+			or gui.Label{ label = " ??? " }
+		}
 	}
+end
+function widgets.ScrollableAwards(fields)
+	local player = fields.player
+	fields.player = nil
+	local bar_width = fields.bar_width
+	fields.bar_width = nil
+	local name = player:get_player_name()
+	local award_list = awards.get_award_states(name)
+	for _, award in ipairs(award_list) do
+		local def = award.def
+		if def:can_unlock(player) and (award.unlocked or not def.secret) then
+			fields[#fields+1] = gui.sway_awards.Award{
+				bar_width = bar_width,
+				award = award
+			}
+		end
+	end
+	return gui.ScrollableVBox(fields)
 end
 sway.register_page("sway_awards:awards", {
 	title = "Awards",
 	bar_width = 6,
+	h = 8.9,
 	get = function(self, player, context)
-		local name = player:get_player_name()
-		local award_list = awards.get_award_states(name)
-		local awards_box = { name = "sway_awards:awards_box", h = 8.9 }
-		for _, award in ipairs(award_list) do
-			local def = award.def
-			if def:can_unlock(player) and (award.unlocked or not def.secret) then
-				table.insert(awards_box, generate_award(self, def.title, def.description, award.unlocked, def.icon, award.progress))
-			end
-		end
-		return sway.widgets.form{
+		return gui.sway.Form{
 			player = player,
 			context = context,
-			gui.ScrollableVBox(awards_box)
+			gui.sway_awards.ScrollableAwards{
+				player = player,
+				bar_width = self.bar_width,
+				h = self.h
+			}
 		}
 	end
 })
