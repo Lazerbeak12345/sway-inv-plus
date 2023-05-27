fb0f005eefe4d69123c03453ad582e8783c2f18a
Nathan Fritzler <nfblaster@live.com>
2023-05-26 22:51:03 -0600

    feat[sway_awards]: Seperate out progress bar.

--- sway_awards/init.lua
+++ sway_awards/init.lua
@@ -1,6 +1,30 @@
 local sway, awards = sway, awards
 local gui = sway.widgets
 sway.mods.sway_awards = { widgets = {} }
+function gui.sway_awards.ProgressBar(fields)
+	local bar_width = fields.w
+	local bar_height = fields.h or 1
+	local current = fields.current
+	local target = fields.target
+	local bg_color = fields.bg_color or "#333333"
+	local color = fields.color or "#008800"
+	return gui.Stack {
+		gui.Box{ w = bar_width, h = bar_height, color = bg_color, align_h = "left" },
+		current > 0 and gui.Box{
+			w = (current * bar_width) / target,
+			h = bar_height,
+			color = color,
+			align_h = "left"
+		} or gui.Nil{},
+		gui.HBox{
+			w = bar_width,
+			align_h = "left",
+			padding = .2,
+			gui.Label{ label = math.floor((current * 100 * 100) / target) / 100 .. "% ", align_h = "left", expand = true },
+			gui.Label{ label = current .. " of " .. target, align_h = "right" } or gui.Nil{}
+		}
+	}
+end
 function gui.sway_awards.Award(fields)
 	local bar_width = fields.bar_width
 	local award = fields.award
@@ -24,22 +48,8 @@ function gui.sway_awards.Award(fields)
 		gui.VBox{
 			gui.Label { label = title },
 			gui.Label { label = description },
-			progress and gui.Stack {
-				gui.Box{ w = bar_width, h = 1, color = "#333333", align_h = "left" },
-				current > 0 and gui.Box{
-					w = (current * bar_width) / target,
-					h = 1,
-					color = "#008800",
-					align_h = "left"
-				} or gui.Nil{},
-				gui.HBox{
-					w = bar_width,
-					align_h = "left",
-					padding = .2,
-					gui.Label{ label = math.floor((current * 100 * 100) / target) / 100 .. "% ", align_h = "left", expand = true },
-					gui.Label{ label = current .. " of " .. target, align_h = "right" }
-				}
-			} or unlocked and gui.Label{ label = "Complete!" }
+			progress and gui.sway_awards.ProgressBar{ w = bar_width, current = current, target = target }
+			or unlocked and gui.Label{ label = "Complete!" }
 			or (not started) and gui.Label{ label = "Not started!" }
 			or gui.Label{ label = " ??? " }
 		}
